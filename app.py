fcce17c1f21932803aa117fc206852e68e63ad89
{chloe} <{trangnguyen.chloe@gmail.com}>
2021-06-20 22:53:17 +0700

    flask app file

--- app.py
+++ app.py
@@ -38,15 +38,6 @@ def parse_image(imgData):
     # return img_decode
     return 'uploads/'+filename
 
-# def preprocess(image):
-#     image = tf.image.decode_jpeg(image, channels=3)
-# #     image = tf.image.resize(image, [192, 192])
-# #     # Use `convert_image_dtype` to convert to floats in the [0,1] range.
-# #     image = tf.image.convert_image_dtype(image, tf.float32)
-# #     image = (image*2) - 1  # normalize to [-1,1] range
-# #     image = tf.image.per_image_standardization(image)
-#     return image
-
 @app.route('/send_image', methods=['POST'])
 def pose_predict():
     data_image = request.get_json()
@@ -54,15 +45,7 @@ def pose_predict():
     name = live_prediction(classifier, classes, image_path)
 
     return jsonify({'label': name}) 
-    # return jsonify({'key':'haha'})
 
 if __name__ == "__main__":
     app.run(debug=True) 
-    
-# def predict(image):
-#     name = live_prediction(classifier, classes, image)
-#     print(name)
 
-# image = os.path.join('D:\Yoga_Companion\my_yoga_time_collection', '76.jpeg')
-# predict(image)
-# print(classes)
